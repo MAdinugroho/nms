cffabe940c0d48e0e3ceddf23691d420d11e7c2e
madinugroho <madinugroho@student.ce.undip.ac.id>
2019-08-22 11:56:45 +0700

    add detail

--- application/config/routes.php
+++ application/config/routes.php
@@ -12,6 +12,7 @@ $route['dashboard'] = 'admin/dashboard';
 $route['webconf'] = 'admin/webconf';
 $route['accountTacac'] = 'admin/accountTacac';
 $route['createAccountTacac/(:any)'] = 'admin/createAccountTacac/$1';
+$route['detailAccountTacac/(:any)'] = 'admin/detailAccountTacac/$1';
 $route['exportXml'] = 'admin/exportXml';
 $route['test'] = 'admin/test';
 $route['404_override'] = '';
