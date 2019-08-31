0faa7a0d648fd2635f31163bd15e973be5dfc707
madinugroho <madinugroho@student.ce.undip.ac.id>
2019-08-31 12:59:18 +0700

    fix top bar

--- application/views/template.php
+++ application/views/template.php
@@ -38,10 +38,7 @@
               <a href="features-profile.html" class="dropdown-item has-icon">
                 <i class="far fa-user"></i> Profile
               </a>
-              <a href="features-activities.html" class="dropdown-item has-icon">
-                <i class="fas fa-bolt"></i> Activities
-              </a>
-              <a href="features-settings.html" class="dropdown-item has-icon">
+              <a href="<?php echo base_url('webconf'); ?>" class="dropdown-item has-icon">
                 <i class="fas fa-cog"></i> Settings
               </a>
               <div class="dropdown-divider"></div>
