9e4c343c88adb1449b27d8a99c534b2b52379353
madinugroho <madinugroho@student.ce.undip.ac.id>
2019-10-12 12:14:32 +0700

    test delete

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -184,8 +184,7 @@ class Admin_model extends CI_Model
       if ($this->input->post('status') == 0) {
         notify('Super Akun Tidak Dapat Dihapus', 'error', 'accountTacac');
       } else {
-				$test = Shell_exec('powershell.exe Remove-ADUser -Identity ' . $this->input->post('username') . ' -Confirm:$false');
-				var_dump($test);die;
+				Shell_exec('powershell.exe Remove-ADUser -Identity ' . $this->input->post('username') . ' -Confirm:$false');
 				sleep(3);
         $this->db->delete('account_tacac', array('id' => $this->input->post('id')));
         $dataLog = 'Delete '.$this->input->post('group').'';
